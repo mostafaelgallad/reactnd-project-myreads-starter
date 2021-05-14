c9281dd64c51550e654ca8e451387fd15fbcb956
Mostafa El Gallad <m.elgallad@outlook.com>
2021-05-14 23:18:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import * as BooksAPI from "./BooksAPI";
-import "./App.css";
-import MainPage from "./Views/MainPage";
 import { Route } from "react-router-dom";
+
+import "./App.css";
+
+import * as BooksAPI from "./BooksAPI";
+import MainPage from "./Views/MainPage";
 import Search from "./components/Search";
-import {} from "react-router-dom";
+
 class BooksApp extends React.Component {
   state = {
     /**
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { search } from "../BooksAPI";
 
+import { search } from "../BooksAPI";
 import Book from "../components/Book";
 
 class Search extends Component {
